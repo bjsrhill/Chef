1cee86ab72d3394a9aae064703405ffc6b4a2304
Beverly S Hill <bjsrhill@gmail.com>
2016-08-14 15:53:12 -0400

    Adding webServer.rb

---
+++ webServer.rb
@@ -0,0 +1,19 @@
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+apt_package 'python-software-properties' do
+end
+
+execute "add-apt-repository ppa:webupd8team/java" do
+  command "add-apt-repository ppa:webupd8team/java"
+end
+
+execute "apt-get update" do
+  command "apt-get update"
+end
+
+execute  'oracle-java7-installer' do
+  command 'apt-get install -y oracle-java7-installer'
+end
