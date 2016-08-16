380e54b707259db9f22b76b31968335f8770358d
Beverly S Hill <bjsrhill@gmail.com>
2016-08-16 18:26:13 -0400

    Added missing do

--- WebServer/recipes/default.rb
+++ WebServer/recipes/default.rb
@@ -33,7 +33,7 @@ execute "Install oracle-java8 accepting license" do
   command "echo 'oracle-java8-installer shared/accepted-oracle-license-v1-1 select true' | sudo /usr/bin/debconf-set-selections"
 end
 
-execute "install oracle java"
+execute "install oracle java" do
   command "apt-get install -y oracle-java8-installer"
 end
 
